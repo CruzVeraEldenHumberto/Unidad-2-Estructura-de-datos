99811ec7c69bc4f7a6e445e3777289b153aa2900
Elden Humberto Cruz Vera <elden.cruz17@tectijuana.edu.mx>
2018-09-28 00:43:37 -0700

    Factorial - For y Recursividad
    listo

---
+++ E2-1For.Cruz Vera Elden Humberto/E2-1.Cruz Vera Elden Humberto/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace E2_1.Cruz_Vera_Elden_Humberto
+{
+    public class Factoriales
+    {
+        public int Factorial = 1;
+
+        public int Calcular()
+        {
+            for (int Contador = 1; Contador < 7; Contador++)
+                Factorial = Factorial * Contador;
+
+            return Factorial;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine("El factorial de 6 es: {0}", Calcular());
+        }
+            
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch tiempo = new Stopwatch(); tiempo.Start();
+            Factoriales fact = new Factoriales();
+            fact.Mostrar();
+            tiempo.Stop();
+
+            Console.WriteLine("\nTiempo de ejecución del programa: {0}", tiempo.Elapsed.ToString());
+            Console.Write("\nPresione una tecla para salir ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ E2-1Recursividad.Cruz Vera Elden Humberto/E2-1Recursividad.Cruz Vera Elden Humberto/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace E2_1Recursividad.Cruz_Vera_Elden_Humberto
+{
+    public class Factoriales
+    {
+        public int Resultado = 0;
+
+        public int Factorial(int FactMenor)
+        {
+            if (FactMenor == 0)
+            {
+                return 1;
+            }
+
+            Resultado = FactMenor * Factorial(FactMenor - 1);
+            return Resultado;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch tiempo = new Stopwatch(); tiempo.Start();
+
+            Factoriales facto = new Factoriales();
+            
+            Console.WriteLine("El factorial de 6 es: {0}", facto.Factorial(6));
+
+            tiempo.Stop();
+            Console.WriteLine("\nTiempo de ejecución del programa: {0}", tiempo.Elapsed.ToString());
+            Console.Write("\nPresione una tecla para salir ");
+            Console.ReadKey();
+        }
+    }
+}
